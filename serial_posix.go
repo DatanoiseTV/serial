41fe62653e0969672dc317e0633e8fb77278e6db
Datanoise <github@ext.no-route.org>
2018-05-24 15:12:46 +0200

    Support higher baud rate of 460800.

--- serial_posix.go
+++ serial_posix.go
@@ -37,6 +37,8 @@ func openPort(name string, baud int, databits byte, parity Parity, stopbits Stop
 	}
 	var speed C.speed_t
 	switch baud {
+	case 460800:
+		speed = 460800
 	case 115200:
 		speed = C.B115200
 	case 57600:
